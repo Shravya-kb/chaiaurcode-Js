e8a1f0cbf72fb31295248c88f90c0e4053bfae92
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-04-28 07:18:16 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail ="shravya@gmail.com";
+var accountPassword = "Jaipur";
+let accountState;
+//javascipt is a dynamically typed language.
+//accountId = 2;// not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword="21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as new version.
+
+//alert(3+3) //we are using node js, not browser
+
+console.log(3
+    +
+    3) //code readability must be high
+console.log("Hitesh")
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigInt
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol =>unique
+
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
