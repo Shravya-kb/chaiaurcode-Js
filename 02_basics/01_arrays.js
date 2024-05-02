77dcd0f531ab4ee8ad0ee98b405b68560f160bbb
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-02 00:27:04 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -21,14 +21,23 @@ SHALLOW COPY => A shallow copy of an object is a copy whose properties share
 */
   const myHeros =["shaktiman","naagraj"];
 
-  const myArr2 = new Array
-  console.log(myArr[0]);
+  const myArr2 = new Array(1, 2, 3, 4);
+  console.log(myArr[1]);
   
+  //Array methods
+
+  myArr.push(6)
+  myArr.push(7)
+  myArr.pop()
+  console.log(myArr);
 
 
 
 
+myArr.unshift(9);
+myArr.shift();
 
+console.log(myArr.includes(9));
+console.log(myArr);
 
-*/
 
