18d27f2348091eec151463beafe94f2e114e7ca4
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-05 10:26:16 +0000

    objects summary

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// singleton
+
+//when we decalre spmething in literals then it willl be singleton.
+
+//object literals
+
+const JsUser = {
+    name: "Hitesh",
+    age: 18,
+    location: "Jaipur",
+    email:"hitesh@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser[email]);
