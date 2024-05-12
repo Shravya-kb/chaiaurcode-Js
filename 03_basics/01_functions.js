7389f263d61d40036dd873972ad54cd97013bd2f
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-12 16:15:01 +0000

    FUNCTIONS Advanced

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -61,3 +61,43 @@ console.log(loginUserMessage("Shravya")); //OUTPUT:Shravya just logged in
 console.log(loginUserMessage(""));//OUTPUT:  just logged in
 console.log(loginUserMessage());//OUTPUT:undefined just logged in
 
+function calculateCartPrice(...num1){//(...num1) is rest operator
+    return num1;
+}
+
+console.log(calculateCartPrice(200,400,500));
+
+function calculateCartPrice1(val1,val2,...num1){//(...num1) is rest operator
+    return num1;
+}
+  
+console.log(calculateCartPrice1(200,400,500));
+
+const user ={
+    username: "hitesh",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+
+//handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+console.log(returnSecondValue(myNewArray));//OUTPUT:100
+
+console.log(returnSecondValue([200,400,500, 1000]));
+
