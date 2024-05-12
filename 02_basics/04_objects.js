6743681b403fe824cd1f4faacde0b9dc654c80e6
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-12 09:36:52 +0000

    objects advanced

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -14,5 +14,14 @@ const regularUser={
         }
     }
 }
-console.log(regularUser.fullname);
+console.log(regularUser.fullname.userFullname.first +" "+ regularUser.fullname.userFullname.last);
 console.log(tinderUser);
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
