ea1068e0a62e6fdce2811b9a754e2c17a3852f83
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-18 05:22:27 +0530

    ForEach Loop

---
+++ 05_iterators/six.js
@@ -0,0 +1,54 @@
+const coding = ["js","python","ruby","cpp","ruby","golang"];
+
+//foreach() ***wont return***
+const values =coding.forEach((items=>{
+  console.log(item);
+})
+
+//filter ****returns****
+
+                             // const coding = ["js","ruby","java","python","cpp"]
+
+// const values = coding.forEach((item) =>{
+//   console.log(item);
+//   return item
+// })
+
+// console.log(values);
+//OUTPUT:
+// js
+// ruby
+// java
+// python
+// cpp
+// undefined
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+//NOTE: FOREACH FUNCTION WILL NOT RETURN VALUE WHEREAS FILTER FUNCTION WILL RETURN VALUES
+// const newNums= myNums.filter((num)=>{
+//   return num>4
+// }
+// );
+
+// console.log(newNums)
+//OUTPUT:
+// [ 5, 6, 7, 8, 9, 10 ]
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums=[]
+
+// myNums.forEach((num)=>{
+//   if(num>4){
+//   newNums.push(num)
+// }
+// })
+
+// console.log(newNums);
+//OUTPUT:
+// [ 5, 6, 7, 8, 9, 10 ]
+
+
+                             
+                             
+                              
