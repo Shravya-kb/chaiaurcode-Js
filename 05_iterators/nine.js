bd24f35b11ee23d6270c7c364c1546fd9bd9be05
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-18 06:20:30 +0530

    REDUCE MOTHOD

---
+++ 05_iterators/nine.js
@@ -0,0 +1,27 @@
+// const array1 = [1,2,3,4];
+
+//0+1+2+3+4
+
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue)=>accumulator + currentValue,
+//   initialValue
+// );
+//   console.log(sumWithInitial);
+  //Expected ouput: 10
+
+
+// const myNums =[1,2,3]
+// const myTotal = myNums.reduce(function(acc, currval){
+//   console.log(`acc:${acc} and currval: ${currval}`)
+//   return acc + currval
+// },0)
+
+// console.log(myTotal);
+
+//OUTPUT:
+// acc:0 and currval: 1
+// acc:1 and currval: 2
+// acc:3 and currval: 3
+// 6
+
