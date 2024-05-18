3c18154958c88f3a712b189f018b3b8adc1cfe51
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-18 06:45:48 +0530

    REDUCE METHOD EXPLAINED

--- 05_iterators/nine.js
+++ 05_iterators/nine.js
@@ -24,4 +24,32 @@
 // acc:1 and currval: 2
 // acc:3 and currval: 3
 // 6
+// const myNums =[1,2,3];
+// const myTotal = myNums.reduce ((acc,curr) =>acc+curr,0)
+// console.log(myTotal);
+//OUTPUT:
+//6
 
+const shoppingCart = [
+  {
+    itemName: "js Course ",
+    price:2999
+  },
+  {
+    itemName: "py Course ",
+    price: 999
+  },
+  {
+    itemName: "mobile dev Course ",
+    price:5999
+  },
+  {
+    itemName: "data science Course ",
+    price:12999
+  },
+]
+
+const priceToPay = shoppingCart.reduce((acc,item)=>acc + item.price, 0)
+console.log(priceToPay);
+
+//OUTPUT:22996
