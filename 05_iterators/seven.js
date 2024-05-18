480704381fa3add0972d4c6045a30cc2f4d7877d
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-18 05:54:03 +0530

    MAP FUNCTIONS EXPLAINED

---
+++ 05_iterators/seven.js
@@ -0,0 +1,39 @@
+//MAP FUNCTION:
+
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumbers.map( (num) => {return num + 10})
+// console.log(newNums);
+//OUTPUT:
+// [
+//   11, 12, 13, 14, 15,
+//   16, 17, 18, 19, 20
+// ]
+
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+//note: we can use multiple methods together that is called chaining.
+
+// const newNums = myNumbers
+//                 .map((num) => num*10)
+//                 .map((num) => num + 1)
+                
+// console.log(newNums);
+
+//OUTPUT:
+// [
+//   11, 21, 31, 41,  51,
+//   61, 71, 81, 91, 101
+// ]
+
+//const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNumbers
+//                 .map((num) => num*10)
+//                 .map((num) => num + 1)
+//                 .filter((num) => num >= 40)
+// console.log(newNums);
+//OUTPUT:
+// [
+//   41, 51,  61, 71,
+//   81, 91, 101
+// ]
+
